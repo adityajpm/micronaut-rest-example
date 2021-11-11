0a7f3ddcbeebc6f112b403af55b8a172a5274d8a
adityaIndexlabs <aditya.sofat@indexlabs.co.uk>
2021-11-11 08:51:08 +0000

    assert for new Transfer RequestId

--- adapters/driving/web/src/test/kotlin/com/jpmorgan/labs/micronaut/rest/example/adapters/driving/web/TransferDetailsEndPointTest.kt
+++ adapters/driving/web/src/test/kotlin/com/jpmorgan/labs/micronaut/rest/example/adapters/driving/web/TransferDetailsEndPointTest.kt
@@ -3,11 +3,13 @@ package com.jpmorgan.labs.micronaut.rest.example.adapters.driving.web
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotEmpty
+import com.jpmorgan.labs.jvm.commons.domain.identity.Ulid
 import com.jpmorgan.labs.jvm.commons.json.schema.test.utils.compliesWith
 import com.jpmorgan.labs.micronaut.rest.example.AccountNumber
 import com.jpmorgan.labs.micronaut.rest.example.Amount
 import com.jpmorgan.labs.micronaut.rest.example.TransferDetails
 import com.jpmorgan.labs.micronaut.rest.example.UserId
+import com.jpmorgan.labs.micronaut.rest.example.application.Application
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.apache.*
@@ -16,8 +18,14 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.util.*
+import io.micronaut.context.annotation.Primary
+import io.micronaut.context.annotation.Replaces
 import io.micronaut.runtime.server.EmbeddedServer
+import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
 import jakarta.inject.Inject
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
@@ -42,18 +50,24 @@ class TransferDetailsEndPointTest {
         }
     }
 
+    private val application: Application = mockk()
+
     @Test
     fun `creating a new transfer request works`()  = runBlocking {
         val transferDetails = TransferDetails(UserId.createFromULid(), Amount(10), AccountNumber("12345678"), AccountNumber("12345679"))
         val transferRequestJson = transferDetails.toJson()
         assertThat(transferRequestJson).compliesWith(Api.Resources.TransferDetails.jsonSchema)
+        val newTransferRequestId = Ulid.create()
+        coEvery { application.transferRequest(transferDetails) } returns newTransferRequestId
 
         val response: HttpResponse = client.post("http://${server.host}:${server.port}/${Api.Endpoints.Transfer.path}"){
             contentType(ContentType.Application.Json)
             body = transferRequestJson.toString()
         }
+        val responseContent = response.receive<String>()
 
         assertThat(response.status).isEqualTo(HttpStatusCode.Created)
+        assertThat(responseContent).isEqualTo(newTransferRequestId.toString())
     }
 
     @Test
@@ -72,5 +86,9 @@ class TransferDetailsEndPointTest {
         assertThat(response.status).isEqualTo(HttpStatusCode.BadRequest)
         assertThat(responseContent).isNotEmpty()
     }
+
+    @MockBean
+    @Replaces(bean = Application::class, qualifier = [Primary::class])
+    fun application(): Application = application
 
 }
